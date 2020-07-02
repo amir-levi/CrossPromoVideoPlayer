f601f7275769f2587f45e4db833f66974a493078
Amir Levi <tiptipon81@gmail.com>
2020-07-02 11:28:02 +0300

    added CrossPromotionVideoPlayerTrack model

---
+++ Assets/SuperSonic/Models/CrossPromotionVideoPlayerTrack.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace SuperSonic.Models
+{
+    [Serializable]
+    public class CrossPromotionVideoPlayerTrack
+    {
+        public int Id;
+        public string VideoUrl;
+        public string ClickUrl;
+        public string TrackingUrl;
+    }
+}
