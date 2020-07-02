90e99abf93cf8f5d74c58389f6dbe7c464eed5e0
Amir Levi <tiptipon81@gmail.com>
2020-07-02 11:28:55 +0300

    created CrossPromotionVideoPlayerWebRequest to fetch data from server and return tracks list model

---
+++ Assets/SuperSonic/Networking/CrossPromotionVideoPlayerWebRequest.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using SuperSonic.Models;
+using UnityEngine;
+using UnityEngine.Networking;
+
+namespace SuperSonic.Networking
+{
+    public static class CrossPromotionVideoPlayerWebRequest
+    {
+        
+        public static async Task<List<CrossPromotionVideoPlayerTrack>> FetchVideoTracksAsync(string url)
+        {
+            var client = new HttpClient();
+            var jsonString = await client.GetStringAsync(url).ConfigureAwait(false);
+            var jsonObject = JsonUtility.FromJson<RootObject> (jsonString);
+            return jsonObject.results.Select(result => new CrossPromotionVideoPlayerTrack {Id = result.id, ClickUrl = result.click_url, TrackingUrl = result.tracking_url, VideoUrl = result.video_url}).ToList();
+        }
+        
+        
+        
+        private class RootObject
+        {
+            [Serializable]
+            public class Result
+            {
+                public int id;
+                public string video_url;
+                public string click_url;
+                public string tracking_url;
+            }
+
+            public Result[] results;
+        }
+       
+    }
+}
