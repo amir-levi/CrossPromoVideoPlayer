10ff3de1c82ed13b42d4d93ddd5b6d11a8a0d61e
Amir Levi <tiptipon81@gmail.com>
2020-07-06 11:27:57 +0300

    this class is in charge For sending and fetching data from the server

--- Assets/CrossPromo/Networking/CrossPromotionVideoPlayerWebRequest.cs
+++ Assets/CrossPromo/Networking/CrossPromotionVideoPlayerWebRequest.cs
@@ -1,14 +1,12 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Net;
 using System.Net.Http;
 using System.Threading.Tasks;
-using SuperSonic.Models;
+using CrossPromo.Models;
 using UnityEngine;
-using UnityEngine.Networking;
 
-namespace SuperSonic.Networking
+namespace CrossPromo.Networking
 {
     public static class CrossPromotionVideoPlayerWebRequest
     {
