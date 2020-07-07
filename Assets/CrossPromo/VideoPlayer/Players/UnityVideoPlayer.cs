3765528f7de1581e5ef04f53936b50d6a67ff7cb
Amir Levi <tiptipon81@gmail.com>
2020-07-07 14:43:53 +0300

    reverted how play a video tracks works, instead having all the logic in UnityVideoPlayer moved the way a track is served to VideoPlayerTray and he server the track that should be played

--- Assets/CrossPromo/VideoPlayer/Players/UnityVideoPlayer.cs
+++ Assets/CrossPromo/VideoPlayer/Players/UnityVideoPlayer.cs
@@ -1,117 +1,100 @@
 using System;
 using System.Collections.Generic;
-using System.Linq;
 using CrossPromo.Models;
 using UnityEngine;
-using UnityEngine.UI;
 
 namespace CrossPromo.VideoPlayer.Players
 {
     [Serializable]
     public class UnityVideoPlayer : CrossPromotionVideoPlayer
     {
-        private RawImage _screen;
-        private int[] _videoTracksIndices; 
-        private VideoPlayerFactory<UnityVideoTrack> _videoPlayerFactory;
-        private UnityVideoTrack _previousPlayingVideoTrack;
-        private UnityVideoTrack _currentPlayingVideoTrack;
-
-        [SerializeField]private int _cacheSize;
-        private const int MaxCacheSize = 3;
+        private VideoPlayerScreen _screen;
+        private VideoPlayerTray _videoPlayerTray;
         
-        public override void Init(List<CrossPromotionVideoPlayerTrack> videoPlayerTracks, RawImage screen)
+        public override void Init(List<CrossPromotionVideoPlayerTrack> videoPlayerTracks, VideoPlayerScreen screen)
         {
             if (videoPlayerTracks == null || videoPlayerTracks.Count <= 0)
             {
-                Debug.LogError("Assign VideoClips from the Editor");
+                Debug.LogError("No Video Player Tracks - Check Data fetch from the server");
                 return;
             }
             
-            VideoPlayerTracks = videoPlayerTracks;
             _screen = screen;
-
-            _cacheSize = videoPlayerTracks.Count < MaxCacheSize ? videoPlayerTracks.Count : MaxCacheSize;
-            
-            _videoTracksIndices = new int[_cacheSize];
-            _videoPlayerFactory = new UnityVideoPlayerFactory(VideoPlayerTracks.Count,transform);
-            RunNextTrack(CurrentPlayedTrackIndex);
-        }
-
-        private void RunNextTrack(int index)
-        {
-            _currentPlayingVideoTrack = _videoPlayerFactory.GetVideoTrack(VideoPlayerTracks[index]);
-            if(_previousPlayingVideoTrack != null)
-                _previousPlayingVideoTrack.Stop();
-            
-            _videoTracksIndices[0] = index;
-            CurrentPlayedTrackIndex = index;
-            
-            _currentPlayingVideoTrack.OnTrackPrepared = () =>
+            _screen.OnClick = () =>
             {
-                _currentPlayingVideoTrack.Play(_screen);
+                OnVideoClicked?.Invoke(_videoPlayerTray.CurrentTrack.CrossPromotionTrack);
             };
-
-            _currentPlayingVideoTrack.OnTrackFinish = () =>
-            {
-                _previousPlayingVideoTrack = _currentPlayingVideoTrack;
-                RunNextTrack(_videoTracksIndices[1]);
-            };
-            
-            StartCoroutine(_currentPlayingVideoTrack.Prepare());
-
-            var nextIndex = (index + VideoPlayerTracks.Count + 1) % VideoPlayerTracks.Count;
-            if(PrepareTrack(index, nextIndex))
-                _videoTracksIndices[1] = nextIndex;
-            
-            var prevIndex = (index + VideoPlayerTracks.Count - 1) % VideoPlayerTracks.Count;
-            if(PrepareTrack(index, prevIndex))
-                _videoTracksIndices[2] = prevIndex;
-        }
-
-        private bool PrepareTrack(int currentIndex, int trackIndexToPrepare)
-        {
-            // check if VideoPlayerTracks doesnt have only 1 element
-            if (trackIndexToPrepare != currentIndex)
-            {
-                // check if VideoPlayerTracks in trackIndexToPrepare is not null
-                if (VideoPlayerTracks.ElementAtOrDefault(trackIndexToPrepare) != null)
-                {
-                    var player  = _videoPlayerFactory.GetVideoTrack(VideoPlayerTracks[trackIndexToPrepare]);
-                    StartCoroutine(player.Prepare());
-                    return true;
-                }
-            }
-
-            return false;
+            _videoPlayerTray = new VideoPlayerTray(videoPlayerTracks,transform);
+            RunTrack(_videoPlayerTray.CurrentTrack);
         }
         
+        private void RunTrack(UnityVideoTrack track)
+        {
+            track.OnTrackPrepared = () =>
+            {
+                track.Play(_screen);
+            };
 
+            track.OnTrackFinish = () =>
+            {
+                track.Stop();
+                var nextTrack = _videoPlayerTray.GetNextTrack();
+                RunTrack(nextTrack);
+            };
+            
+            StartCoroutine(track.Prepare());
+            _videoPlayerTray.UpdateTray(track);
+
+            if (_videoPlayerTray.NextTrack != null)
+            {
+                _videoPlayerTray.NextTrack.OnTrackPrepared = () =>
+                {
+                    OnNextVideoTrackReady?.Invoke();
+                };
+                StartCoroutine(_videoPlayerTray.NextTrack.Prepare());
+            }
+            
+            if (_videoPlayerTray.PreviousTrack != null)
+            {
+
+                if (_videoPlayerTray.PreviousTrack != _videoPlayerTray.NextTrack)
+                {
+                    _videoPlayerTray.PreviousTrack.OnTrackPrepared = () =>
+                    {
+                        OnPreviousVideoTrackReady?.Invoke();
+                    };
+                    StartCoroutine(_videoPlayerTray.PreviousTrack.Prepare());
+                }
+            }
+        }
 
         public override void Next()
         {
-            _previousPlayingVideoTrack = _currentPlayingVideoTrack;
-            RunNextTrack(_videoTracksIndices[1]);
+            _videoPlayerTray.CurrentTrack.Stop();
+            var track = _videoPlayerTray.GetNextTrack();
+            RunTrack(track);
         }
 
         public override void Previous()
         {
-            _previousPlayingVideoTrack = _currentPlayingVideoTrack;
-            RunNextTrack(_videoTracksIndices[2]);
+            _videoPlayerTray.CurrentTrack.Stop();
+            var track = _videoPlayerTray.GetPreviousTrack();
+            RunTrack(track);
         }
 
         public override void Pause()
         {
-            _currentPlayingVideoTrack.Pause();
+          _videoPlayerTray.CurrentTrack.Pause();
         }
 
         public override void Resume()
         {
-            _currentPlayingVideoTrack.Resume();
+           _videoPlayerTray.CurrentTrack.Resume();
         }
 
         public override bool IsPlaying()
         {
-            return false;
+           return _videoPlayerTray.CurrentTrack.IsPlaying();
         }
        
     }
