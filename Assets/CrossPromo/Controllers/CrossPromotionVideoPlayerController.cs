ddee5a202bdc0efde5c4431667efaf428bf649eb
Amir Levi <tiptipon81@gmail.com>
2020-07-07 14:57:22 +0300

    registered to VideoClicked event

--- Assets/CrossPromo/Controllers/CrossPromotionVideoPlayerController.cs
+++ Assets/CrossPromo/Controllers/CrossPromotionVideoPlayerController.cs
@@ -9,11 +9,19 @@ namespace CrossPromo.Controllers
     {
         private readonly CrossPromotionVideoPlayerView _videoPlayerView;
         
-        
         public CrossPromotionVideoPlayerController()
         {
             _videoPlayerView = GameObject.FindGameObjectWithTag("CrossPromotionVideoPlayer").GetComponent<CrossPromotionVideoPlayerView>();
+            _videoPlayerView.VideoClicked = (instanceId, track) =>
+            {
+                var trackingUrl = track.TrackingUrl;
+                var clickUrl = track.ClickUrl;
+            
+                Debug.Log($"instanceId: {instanceId} trackingUrl: {trackingUrl}  clickUrl: {clickUrl} ");
+            };
         }
+
+       
 
 
         public async void CrossPromotionVideoPlayerView(Type videoPlayerType,string serverUrl)
