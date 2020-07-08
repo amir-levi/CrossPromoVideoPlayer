6ae62260c29ac9e50c0c933fd32b51bc8939b689
Amir Levi <tiptipon81@gmail.com>
2020-07-08 15:58:46 +0300

    clean up code

--- Assets/CrossPromo/Startup.cs
+++ Assets/CrossPromo/Startup.cs
@@ -6,12 +6,11 @@ namespace CrossPromo
 {
     public class Startup : MonoBehaviour
     {
-        [SerializeField] private string ServerUrl;
+      
         
         private void Start() 
         {
-            new CrossPromotionVideoPlayerController().CrossPromotionVideoPlayerView(typeof(UnityVideoPlayer),ServerUrl);
-           // new MockCrossPromoController().CrossPromotionVideoPlayerView(typeof(UnityVideoPlayer),ServerUrl, MockCrossPromoController.TrackOptions.One);
+            new CrossPromoVideoPlayerController().CrossPromotionVideoPlayerView(typeof(CrossPromoVideoPlayer));
         }
         
     }
