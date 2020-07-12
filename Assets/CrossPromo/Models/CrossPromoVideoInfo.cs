7d9a1c7892db7691f731a02ae8782a07f9d6c4d6
Amir Levi <tiptipon81@gmail.com>
2020-07-12 13:11:02 +0300

    override ToString method

--- Assets/CrossPromo/Models/CrossPromoVideoInfo.cs
+++ Assets/CrossPromo/Models/CrossPromoVideoInfo.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Text;
 
 namespace CrossPromo.Models
 {
@@ -10,5 +11,17 @@ namespace CrossPromo.Models
         public string LocalPath;
         public string ClickUrl;
         public string TrackingUrl;
+
+
+        public override string ToString()
+        {
+            var sb = new StringBuilder();
+            sb.AppendLine($"Id: {Id}");
+            sb.AppendLine($"VideoUrl: {VideoUrl}");
+            sb.AppendLine($"LocalPath: {LocalPath}");
+            sb.AppendLine($"TrackingUrl: {TrackingUrl}");
+    
+            return sb.ToString();
+        }
     }
 }
